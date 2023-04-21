943ca767263670f398b32b6bafca4273978d66d3
Katherine Yang <whykatherine@gmail.com>
2023-04-21 15:37:56 -0400

    use env for port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -16,6 +18,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('listening on *:3000');
 });
