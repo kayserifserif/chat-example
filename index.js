d7c69b391f5fb28d620f4986221f8370cf2e15ae
Katherine Yang <whykatherine@gmail.com>
2023-04-21 21:43:43 -0400

    body parser

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const bodyParser = require('body-parser');
 const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/post', (req, res) => {
+// app.post('/post', cors(), (req, res) => {
+  console.log("content-type", req.headers['content-type']);
+  console.log("request body", req.body);
+  // console.log("title received: " + req.body.title);
+  res.send("welcome to post");
 });
 
 io.on('connection', (socket) => {
@@ -19,5 +35,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log('listening on *:3000');
+  console.log(`listening on *:${PORT}`);
 });
